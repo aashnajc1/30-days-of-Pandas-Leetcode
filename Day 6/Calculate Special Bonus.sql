f32242c5ee299f5b67131f96c16dd99db741cde9
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-08-17 17:07:19 +0530

    ,

---
+++ Day 6/Calculate Special Bonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select employee_id,
+CASE
+    WHEN employee_id % 2 <> 0 and name not like "M%" THEN  salary
+    ELSE 0
+END AS bonus
+from Employees
+order by employee_id;
